b92ca8abdf80929c3d2fd348c1babdad1a497ea7
kaushik Visvanathan <kaushik.316@gmail.com>
2018-01-19 18:43:31 -0500

    added folder for data

--- src/get_reviews.py
+++ src/get_reviews.py
@@ -3,7 +3,6 @@ import requests
 import pprint 
 import json
 
-
 url = "http://api.penncoursereview.com/v1/coursehistories/JWST-162/reviews?token={}".format(api_key)
 r = requests.get(url)
 print(r.text)
