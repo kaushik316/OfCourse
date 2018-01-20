0d8335e4e30ec9daa89eb412b6668a074512600e
bheath015 <31784768+bheath015@users.noreply.github.com>
2018-01-20 12:02:46 -0500

    i built the whole app because i am good at computers

--- Site/routes.py
+++ Site/routes.py
@@ -10,7 +10,33 @@ app = Flask(__name__)
 #  The main view of CryptoGuru Invemestment Analyzer
 @app.route('/', methods = ['GET', 'POST'])
 def landing():
-    return render_template('landing.html')
+  input_major = request.form.get('major')
+  input_kw = request.form.get('kw')
+  input_course_diff = request.form.get('course_diff')
+  input_inst_acc = request.form.get('inst_acc')
+  input_stud_int = request.form.get('stud_int')
+  input_value_read = request.form.get('value_read')
+  input_work_req = request.form.get('work_req')
+  if not input_kw:
+    input_kw = 'test_input'
+  if not input_course_diff:
+    input_course_diff = 3
+  if not input_inst_acc:
+    input_inst_acc = 3
+  if not input_stud_int:
+    input_stud_int = 3
+  if not input_value_read:
+    input_value_read = 3
+  if not input_work_req:
+    input_work_req = 3
+  return render_template('landing.html', 
+    input_major = str(input_major),
+    input_kw = str(input_kw),
+    input_course_diff = str(input_course_diff),
+    input_inst_acc = str(input_inst_acc),
+    input_stud_int = str(input_stud_int),
+    input_value_read = str(input_value_read),
+    input_work_req = str(input_work_req))
 
 if __name__=='__main__':
   app.run(debug=True)
