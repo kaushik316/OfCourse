d9efdfd62110297175038716e4b0abdaed2b379f
kaushik Visvanathan <kaushik.316@gmail.com>
2018-01-19 19:13:30 -0500

    updated review grabbing code

--- src/get_reviews.py
+++ src/get_reviews.py
@@ -5,4 +5,24 @@ import json
 
 url = "http://api.penncoursereview.com/v1/coursehistories/JWST-162/reviews?token={}".format(api_key)
 r = requests.get(url)
-print(r.text)
+
+# load api response into a dictionary
+data = json.loads(r.text)
+reviews = data["result"]["values"]
+print reviews[0]
+
+# store course information to be used as features
+review_dict = {}
+features = ['Difficulty', 'StimulateInterest', 'RecommendMajor','rWorkRequired', 'InstructorAccess', 'ReadingsValue', 'RecommendNonMajor']
+
+for feature in features:
+	review_dict[feature] = []
+
+# iterate through all reviews for a given course and store features
+for review in reviews:
+	for feature in features:
+		rating = review_dict['ratings'][feature]
+		review_dict[feature].append(rating)
+
+
+print review_dict
