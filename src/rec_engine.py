6c41b3bbfac0ae097e63e90b9e1bf22e09c295cd
kaushik Visvanathan <kaushik.316@gmail.com>
2018-01-20 16:51:41 -0500

    Added recommend_course.py

--- src/rec_engine.py
+++ src/rec_engine.py
@@ -12,17 +12,10 @@ import numpy as np
 import pickle
 
 
-def return_random_sample(df):
-	course = df.sample(n=1)
-	course_alias = course['Course Alias'].iloc[0]
-	course_name = course['Course Name'].iloc[0]
-	return (course_alias, course_name)
-
 # To save model and dataframes
 def pickle_stuff(obj, path):
 	with open(path, 'wb') as p_file:
 		pickle.dump(obj, p_file)
-
 
 model_df = pd.read_csv('Data/allcourses_proc.csv')
 
@@ -65,18 +58,8 @@ svm_score = svm_clf.score(x_test, y_test)
 
 print "score is {}".format(svm_score)
 
-# Test a prediction on a synthetic sample
-random_vec = np.asarray([2.16, 3.68, 3.55, 3.5, 2.32])
-random_vec = random_vec.reshape(1, -1)
-class_pred = svm_clf.predict(random_vec)[0]
-
-# Create a subset of courses that match the preferences
-pred_df = final_df.loc[final_df['Label'] == class_pred]
-
-alias, name = return_random_sample(pred_df)
-print "The recommended course is {}".format(name)
-print "Course ID: {}".format(alias)
-
+# Refit on entire training data
+svm_clf.fit(X, y)
 
 # Save model and final dataframe
 pickle_stuff(svm_clf, "Pickled/svm.pickle")
---
+++ src/recommend_course.py
@@ -0,0 +1,39 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+
+def return_random_sample(df):
+	course = df.sample(n=1)
+	course_alias = course['Course Alias'].iloc[0]
+	course_name = course['Course Name'].iloc[0]
+	return (course_alias, course_name)
+
+def filter_major(df):
+	pass
+
+def return_course(input_array):
+	with open('Pickled/svm.pickle', 'rb') as model_file:
+		svm_clf = pickle.load(model_file)
+
+	with open('Pickled/final_df.pickle', 'rb') as df_file:
+		final_df = pickle.load(df_file)
+
+	# Predict label
+	class_pred = svm_clf.predict(input_array)[0]
+	print class_pred
+
+	# Create a subset of courses that match the preferences
+	pred_df = final_df.loc[final_df['Label'] == class_pred]
+
+	# Return a random course from the matched subset
+	alias, name = return_random_sample(pred_df)
+	print "The recommended course is {}".format(name)
+	return alias
+
+
+if __name__ == "__main__":
+	random_vec = np.asarray([2.16, 3.68, 3.55, 3.5, 2.32])
+	random_vec = random_vec.reshape(1, -1)
+	recommended_course = return_course(random_vec)
+	print "Course ID: {}".format(recommended_course)
